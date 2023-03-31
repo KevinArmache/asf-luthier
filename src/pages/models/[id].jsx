a486b21b5d5c9062d600149c3fc717c3b4658a50
KevinArmache <kevinarmache@gmail.com>
2023-03-31 15:58:42 +0100

    fix: ssg render

--- src/pages/models/[id].jsx
+++ src/pages/models/[id].jsx
@@ -1,43 +1,15 @@
 import React from "react";
 import { useRouter } from "next/router";
 import WebsiteLayout from "@/layouts/WebsiteLayout";
+import pics from "../../json/images.json";
 import Image from "next/image";
-import data from "../../json/data.json";
+import modelsList from "../../json/data.json";
 
-const id = ({ modelslist }) => {
-  console.log(modelslist);
-  console.log(modelslist.name);
+const id = ({ model }) => {
   const router = useRouter();
   if (router.isFallback) {
     return <h1>Loading</h1>;
   }
-
-  // const {
-  //   id,
-  //   name,
-  //   cover,
-  //   categorie,
-  //   length,
-  //   top,
-  //   weight,
-  //   width,
-  //   description,
-  //   picturesCourtesy,
-  //   images,
-  // } = router.query;
-
-  // function findAlbum(array) {
-  //   let album = {};
-  //   for (let i = 0; i < array.length; i++) {
-  //     if (array[i].name === name) {
-  //       album = array[i];
-  //     }
-  //   }
-
-  //   return album;
-  // }
-  // const pictures = findAlbum(pics);
-  // const onlyimage = pictures.images;
 
   return (
     <WebsiteLayout>
@@ -52,7 +24,7 @@ const id = ({ modelslist }) => {
             className="gallery__selector"
           />
           <Image
-            src={modelslist.cover}
+            src={model.cover}
             alt="guitare-picture"
             width={500}
             height={500}
@@ -60,7 +32,7 @@ const id = ({ modelslist }) => {
           />
           <label htmlFor={`img-0`} className="gallery__thumb">
             <Image
-              src={modelslist.cover}
+              src={model.cover}
               alt="miniature-guitare"
               width={100}
               height={100}
@@ -68,25 +40,25 @@ const id = ({ modelslist }) => {
           </label>
         </div>
 
-        {modelslist.images?.map((item) => {
+        {model.images?.map((image) => {
           return (
-            <div key={item.id} className="gallery__item">
+            <div key={image.id} className="gallery__item">
               <input
                 type="radio"
-                id={`img-${item.id}`}
+                id={`img-${image.id}`}
                 name="gallery"
                 className="gallery__selector"
               />
               <Image
                 className="gallery__img rounded-t-lg mt-5"
-                src={item.src}
+                src={image.src}
                 alt="guitare-picture"
                 width={500}
                 height={500}
               />
-              <label htmlFor={`img-${item.id}`} className="gallery__thumb">
+              <label htmlFor={`img-${image.id}`} className="gallery__thumb">
                 <Image
-                  src={item.src}
+                  src={image.src}
                   alt="miniature-guitare"
                   width={100}
                   height={100}
@@ -95,132 +67,40 @@ const id = ({ modelslist }) => {
             </div>
           );
         })}
-        {/* <div className="gallery__item">
-          <input
-            type="radio"
-            id="img-2"
-            name="gallery"
-            className="gallery__selector"
-          />
-          <Image
-            className="gallery__img "
-            src={cover}
-            alt=""
-            width={400}
-            height={400}
-          />
-          <label htmlFor="img-2" className="gallery__thumb">
-            <Image src={cover} alt="" width={100} height={100} />
-          </label>
-        </div>
-        <div className="gallery__item">
-          <input
-            type="radio"
-            id="img-3"
-            name="gallery"
-            className="gallery__selector"
-          />
-          <Image
-            className="gallery__img"
-            src={cover}
-            alt=""
-            width={400}
-            height={400}
-          />
-          <label htmlFor="img-3" className="gallery__thumb">
-            <Image src={cover} alt="" width={100} height={100} />
-          </label>
-        </div>
-        <div className="gallery__item">
-          <input
-            type="radio"
-            id="img-4"
-            name="gallery"
-            className="gallery__selector"
-          />
-          <Image
-            className="gallery__img"
-            src={cover}
-            alt=""
-            width={400}
-            height={400}
-          />
-          <label htmlFor="img-4" className="gallery__thumb">
-            <Image src={cover} alt="" width={100} height={100} />
-          </label>
-        </div>
-        <div className="gallery__item">
-          <input
-            type="radio"
-            id="img-5"
-            name="gallery"
-            className="gallery__selector"
-          />
-          <Image
-            className="gallery__img"
-            src={cover}
-            alt=""
-            width={400}
-            height={400}
-          />
-          <label htmlFor="img-5" className="gallery__thumb">
-            <Image src={cover} alt="" width={100} height={100} />
-          </label>
-        </div>
-        <div className="gallery__item">
-          <input
-            type="radio"
-            id="img-6"
-            name="gallery"
-            className="gallery__selector"
-          />
-          <Image
-            className="gallery__img"
-            src={cover}
-            alt=""
-            width={400}
-            height={400}
-          />
-          <label htmlFor="img-6" className="gallery__thumb">
-            <Image src={cover} alt="" width={100} height={100} />
-          </label>
-        </div>*/}
       </section>
       {/* DETAILs */}
       <h2 className="text-2xl text-color-secondary m-5 p-5 border-b-1 border-black font-bold">
-        {modelslist.name}
+        {model.name}
       </h2>
       <h3 className="text-xl font-bold text-color-secondary">Categorie</h3>
-      <span className="text-color-secondary text-lg">
-        {modelslist.categorie}
-      </span>
+      <span className="text-color-secondary text-lg">{model.categorie}</span>
       <h3 className="text-xl font-bold text-color-secondary ">Description</h3>
       <p className="m-2 md:w-40vw sm:w-70vw text-lg text-center text-color-secondary ">
-        {modelslist.description}
+        {model.description}
       </p>
       <h3 className="text-xl font-bold text-color-secondary">Details</h3>
       <div className="container-details text-center text-color-secondary">
         <span className="text-color-secondary text-lg">
           {" "}
-          Nut width: {modelslist.nutWidth}
+          Nut width: {model.width}
         </span>
         <br />
         <span className="text-color-secondary text-lg">
           {" "}
-          Scale length: {modelslist.scaleLength}
+          Scale length: {model.scaleLength}
         </span>
         <br />
         <span className="text-color-secondary text-lg">
           {" "}
-          Weight: {modelslist.weight}
+          Weight: {model.weight}
         </span>
         <br />
-        <span className="text-color-secondary text-lg"> Top: {data.top}</span>
+        <span className="text-color-secondary text-lg"> Top: {model.top}</span>
         <br />
         <br />
         <br />
         <span className="text-color-secondary text-lg">
-          Pictures Courtesy of {modelslist.picturesCourtesy}
+          Pictures Courtesy of {model.picturesCourtesy}
         </span>
       </div>
     </WebsiteLayout>
@@ -232,10 +112,9 @@ const id = ({ modelslist }) => {
 // Generates `/posts/1` and `/posts/2`
 export async function getStaticPaths() {
   // const parse = JSON.parse(data);
-  let alldata = data;
-  const paths = await alldata.map((path) => {
+  const paths = modelsList.map((model) => {
     return {
-      params: { id: path.id.toString() },
+      params: { id: model.id.toString() },
     };
   });
 
@@ -249,17 +128,10 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   const slug = context.params.id;
 
-  // if (!slug) {
-  //   return {
-  //     notFound: true,
-  //   };
-  // }
-
-  // let data2 = data[slug];
-  let filter = data.filter((element) => element.id === slug);
+  let model = modelsList.find((element) => element.id === slug);
   return {
     // Passed to the page component as props
-    props: { modelslist: filter || null },
+    props: { model },
   };
 }
 
