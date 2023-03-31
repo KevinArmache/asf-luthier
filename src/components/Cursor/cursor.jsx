104aebdb70cacf1c9a696f74e106ee64a0661d2b
KevinArmache <kevinarmache@gmail.com>
2023-03-31 19:13:05 +0100

    add: custom cursor

---
+++ src/components/Cursor/cursor.jsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import React from "react";
+
+const cursor = () => {
+  // UPDATE: I was able to get this working again... Enjoy!
+  useEffect(() => {
+    var cursor = document.querySelector(".cursor");
+    var cursorinner = document.querySelector(".cursor2");
+    var a = document.querySelectorAll("a");
+
+    document.addEventListener("mousemove", function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
+      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+    });
+
+    document.addEventListener("mousemove", function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
+      cursorinner.style.left = x + "px";
+      cursorinner.style.top = y + "px";
+    });
+
+    document.addEventListener("mousedown", function () {
+      cursor.classList.add("click");
+      cursorinner.classList.add("cursorinnerhover");
+    });
+
+    document.addEventListener("mouseup", function () {
+      cursor.classList.remove("click");
+      cursorinner.classList.remove("cursorinnerhover");
+    });
+
+    a.forEach((item) => {
+      item.addEventListener("mouseover", () => {
+        cursor.classList.add("hover");
+      });
+      item.addEventListener("mouseleave", () => {
+        cursor.classList.remove("hover");
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <section>
+        {/* <h1>Custom Cursor</h1>
+        <a href="#">Hover Me</a> */}
+      </section>
+      <div className="cursor"></div>
+      <div className="cursor2"></div>
+    </>
+  );
+};
+
+export default cursor;
