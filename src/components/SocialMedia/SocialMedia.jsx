9fa901c094a0a17f24460552cec6bb6680986ad6
KevinArmache <kevinarmache@gmail.com>
2023-04-16 16:31:40 +0100

    fix: error with icon mail

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -90,6 +90,28 @@ const SocialMedia = () => {
             height="100"
           />
         </a>
+        <div className="icon-hidden">
+          <br />
+          <a href="mailto:savary.adrien@gmail.com">
+            <Icon
+              className="h-7 w-auto block mx-auto"
+              icon="line-md:email"
+              color="#000000"
+              width="100"
+              height="100"
+            />
+          </a>
+          <br />
+          <a href="tel:+33785392044">
+            <Icon
+              className="h-8 ml-1 w-100% block mx-auto"
+              icon="line-md:account-add"
+              color="#000000"
+              width="100"
+              height="100"
+            />
+          </a>
+        </div>
       </div>
       <div className="flex flex-col w-1 mx-auto">
         <span className="border-1 w-0.5 border-black"></span>
